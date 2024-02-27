da5346f98af34d2e02cc8814e0a6c2280d764be6
TejaMaheswari <160056170+TejaMaheswari@users.noreply.github.com>
2024-02-27 15:07:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-  document.getElementById("para").style.color = "red";
-  document.getElementById("para2").style.color = "red";
-  document.getElementById("para3").style.color = "red";
-  document.getElementById("list").style.color = "red";
-}
